6517be2d015a3139bc6aaa12b5920d9e05c97933
Jose Beltran <jbeltran@doterra.com>
2019-04-11 21:04:40 -0600

    post modifcation

--- final-project/js/post.js
+++ final-project/js/post.js
@@ -1,43 +1,17 @@
-// Create a "close" button and append it to each list item
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    myNodelist[i].appendChild(span);
-}
-
-// Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-    close[i].onclick = function () {
-        var div = this.parentElement;
-        div.style.display = "none";
-    }
-}
-
-// Add a "checked" symbol when clicking on a list item
-var list = document.querySelector('ul');
-list.addEventListener('click', function (ev) {
-    if (ev.target.tagName === 'LI') {
-        ev.target.classList.toggle('checked');
-    }
-}, false);
-
-// Create a new list item when clicking on the "Add" button
-function newElement() {
+function newToDoey() {
+    //variables
     var li = document.createElement("li");
     var inputValue = document.getElementById("myInput").value;
-    var t = document.createTextNode(inputValue);
-    li.appendChild(t);
+    var theText = document.createTextNode(inputValue);
+
+    //append, attributes & class name
+    li.appendChild(theText);
     li.className = "list-item";
     li.setAttribute('draggable', 'true');
 
+    //attach the new li to the ul
     if (inputValue === '') {
-        alert("You must write something!");
+        alert("TO-DOey is empty");
     } else {
         document.getElementById("ulList").appendChild(li);
     }
@@ -56,3 +30,32 @@ function newElement() {
         }
     }
 }
+
+
+var myList = document.getElementsByTagName("li");
+var i;
+for (i = 0; i < myList.length; i++) {
+    var span = document.createElement("span");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    myList[i].appendChild(span);
+}
+
+
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
+}
+
+
+var list = document.querySelector('ul');
+list.addEventListener('click', function (e) {
+    if (e.target.tagName === 'li') {
+        e.target.classList.toggle('checked');
+    }
+}, false);
